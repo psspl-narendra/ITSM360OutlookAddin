9d16b344574dd44a8a02e895a5b0dc63241be2ce
Narendra Bariya <narendra@prakashinfotech.com>
2024-12-20 09:57:21 +0530

    Sign logic implements

---
+++ src/launchevent/authconfig.js
@@ -0,0 +1,5 @@
+/* eslint-disable  */
+export const auth = {
+    clientId: "84b64db4-7c90-4cf7-b809-864eccd24379",
+    authority: "https://login.microsoftonline.com/47165d17-0fdb-4f2a-a567-116783fae3a5",
+};
